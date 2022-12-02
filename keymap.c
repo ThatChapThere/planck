97327b9ca07c166a1c7d366fa09bd0af750f7a51
Tim <jamespsmith742@gmail.com>
2022-12-02 00:56:28 +0000

    Uploaded files from qmk on linux

--- keymap.c
+++ keymap.c
@@ -17,7 +17,6 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
-
 enum planck_layers {
   _QWERTY,
   _COLEMAK,
@@ -28,7 +27,8 @@ enum planck_layers {
   _ADJUST,
   _LEFT_MODS,
   _RIGHT_MODS,
-  _NUMPAD
+  _NUMPAD,
+  _MINECRAFT
 };
 
 enum planck_keycodes {
@@ -53,14 +53,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   \  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  #   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Caps |Super |      |      | Enter/Lower | Space/Raise |      |      |Numpad| Menu |
+ * |      |Super |      |      | Enter/Lower | Space/Raise |      |      |Numpad|  MC  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,                 KC_T,               KC_Y,    KC_U,                  KC_I,      KC_O,     KC_P,    KC_BSPC,
-      KC_TAB,  KC_A,    KC_S,    KC_D,    LT(_LEFT_MODS, KC_F), KC_G,               KC_H,    LT(_RIGHT_MODS, KC_J), KC_K,      KC_L,     KC_SCLN, KC_QUOT,
-      KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,                 KC_B,               KC_N,    KC_M,                  KC_COMM,   KC_DOT,   KC_SLSH, KC_BSLS,
-      KC_CAPS, KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX,              LT(_LOWER, KC_ENT), XXXXXXX, LT(_RAISE, KC_SPC),    XXXXXXX,   XXXXXXX,  TG(3),   KC_APP
+      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,                 KC_T,               KC_Y,    KC_U,                  KC_I,      KC_O,     KC_P,          KC_BSPC,
+      KC_TAB,  KC_A,    KC_S,    KC_D,    LT(_LEFT_MODS, KC_F), KC_G,               KC_H,    LT(_RIGHT_MODS, KC_J), KC_K,      KC_L,     KC_SCLN,       KC_QUOT,
+      XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,                 KC_B,               KC_N,    KC_M,                  KC_COMM,   KC_DOT,   KC_SLSH,       KC_BSLS,
+      XXXXXXX, KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX,              LT(_LOWER, KC_ENT), XXXXXXX, LT(_RAISE, KC_SPC),    XXXXXXX,   XXXXXXX,  TG(_NUMPAD),   TG(_MINECRAFT)
 ),
 
 /* Colemak
@@ -101,38 +101,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      | Home | Pg Dn| Pg Up|  End |  Ins |  Del | Left | Down |  Up  | Right|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |   \  |   @  |   £  |   ~  | Menu | Caps |   |  |   %  |   ^  |   ?  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |             |             |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_INS, KC_DEL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______,
-      _______, KC_TILD, XXXXXXX, XXXXXXX, RALT(KC_4), XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE, _______,
-      _______, _______, _______, _______, _______, _______, _______, LT(_ADJUST, KC_SPC), _______, _______, XXXXXXX, _______
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,         KC_F9,   KC_F10,  KC_F11,  KC_F12,
+      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  KC_DEL,  KC_LEFT,       KC_DOWN, KC_UP,   KC_RGHT, _______,
+      _______, KC_NUBS, KC_DQUO, KC_HASH, KC_PIPE, KC_APP,  KC_CAPS, LSFT(KC_NUBS), KC_PERC, KC_CIRC, KC_QUES, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, XXXXXXX, _______
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |   <  |   >  |   (  |   )  |   _  |   =  |   {  |   }  |   [  |   ]  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |   !  |   "  |   #  |   $  |   ¬  |   €  |   &  |   *  |   -  |   +  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |             |             |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-      KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______,
-      _______, KC_LT, KC_GT, KC_LPRN, KC_RPRN, KC_NUBS, KC_EQL, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
-      _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, _______,
-      _______, _______, _______, _______, _______, LT(_ADJUST, KC_UNDS), _______, _______, _______, _______, KC_NO, _______
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    _______,
+      _______, KC_LT,   KC_GT,   KC_LPRN, KC_RPRN, KC_UNDS, KC_EQL,     KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
+      _______, KC_EXLM, KC_AT,   KC_NUHS, KC_DLR,  KC_TILD, RALT(KC_4), KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, _______,
+      _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, XXXXXXX, _______
 ),
 
 /* Plover layer (http://opensteno.org)
@@ -173,10 +173,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_NUMPAD] = LAYOUT_planck_grid(
-      TG(3), KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _______,
-      _______, KC_P0, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _______,
-      _______, KC_PAST, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_NUM, KC_NO, _______, _______, KC_NO, _______,
-      _______, _______, _______, _______, KC_NO, KC_ENT, KC_NO, KC_SPC, _______, _______, KC_NO, _______
+      TG(_NUMPAD), KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PMNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+      _______, KC_P0, KC_P4, KC_P5, KC_P6, KC_PPLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+      _______, KC_PAST, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_NUM, XXXXXXX, _______, _______, XXXXXXX, _______,
+      _______, _______, _______, _______, XXXXXXX, KC_ENT, XXXXXXX, KC_SPC, _______, _______, XXXXXXX, _______
 ),
 
 [_LEFT_MODS] = LAYOUT_planck_grid(
@@ -191,6 +191,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT, KC_RCTL, KC_LALT, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+/* Minecraft
+ * ,-----------------------------------------------------------------------------------.
+ * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  #   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |Super |      |      |    Space    |    Enter    |      |      |Numpad| Menu |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_MINECRAFT] = LAYOUT_planck_grid(
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______,
+      KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______,
+      XXXXXXX, _______, _______, _______, _______, KC_SPC,  _______, KC_ENT,  _______, TG(_MINECRAFT), _______, _______
 )
 
 };
