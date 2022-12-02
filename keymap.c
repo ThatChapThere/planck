614da1143d136d74e6dafac81b473bed25464f3d
ThatChapThere <jamespsmith742@gmail.com>
2022-12-02 01:05:13 +0000

    fixed test

--- keymap.c
+++ keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-      KC_ESC,  KC_R,    KC_W,    KC_E,    KC_R,                 KC_T,               KC_Y,    KC_U,                  KC_I,      KC_O,     KC_P,          KC_BSPC,
+      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,                 KC_T,               KC_Y,    KC_U,                  KC_I,      KC_O,     KC_P,          KC_BSPC,
       KC_TAB,  KC_A,    KC_S,    KC_D,    LT(_LEFT_MODS, KC_F), KC_G,               KC_H,    LT(_RIGHT_MODS, KC_J), KC_K,      KC_L,     KC_SCLN,       KC_QUOT,
       XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,                 KC_B,               KC_N,    KC_M,                  KC_COMM,   KC_DOT,   KC_SLSH,       KC_BSLS,
       XXXXXXX, KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX,              LT(_LOWER, KC_ENT), XXXXXXX, LT(_RAISE, KC_SPC),    XXXXXXX,   XXXXXXX,  TG(_NUMPAD),   TG(_MINECRAFT)
